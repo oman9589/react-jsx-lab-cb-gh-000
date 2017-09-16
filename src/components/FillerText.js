e531294f50a44330569fb0c1e05b390996915ff6
Oliver Spring <oliver9589@gmail.com>
2017-09-16 23:07:16 +0000

    automatically backed up by learn

--- src/components/FillerText.js
+++ src/components/FillerText.js
@@ -3,7 +3,7 @@ import React from 'react';
 class FillerText extends React.Component {
   render() {
     return (
-      <p>'I am a filler text. I can be used to fill your screen. Amazing!</p>
+      <p>I am a filler text. I can be used to fill your screen. Amazing!</p>
     )
   }
 }
