38e8ae31b16d655f8579476303e167780b2b82fb
Oliver Spring <oliver9589@gmail.com>
2017-09-16 23:09:33 +0000

    automatically backed up by learn

--- src/components/RegistrationForm.js
+++ src/components/RegistrationForm.js
@@ -3,13 +3,11 @@ import React from 'react';
 class RegistrationForm extends React.Component {
   render() {
     return (
-      <div>
         <form>
           <input type="text"/>
           <input type="password"/>
-        </form>
         <button type="submit"/>
-      </div>
+        </form>
     )
   }
 }
