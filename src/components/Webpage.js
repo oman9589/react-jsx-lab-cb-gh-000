d6ab1421fe9ec3b02b1889b2d1b271ea9ac602f9
Oliver Spring <oliver9589@gmail.com>
2017-09-16 23:07:45 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -5,7 +5,7 @@ class Webpage extends React.Component {
   render() {
     return (
       <div className='page'>
-        <h1 text="The worlds coolest webpage"/>
+        <h1>The worlds coolest webpage</h1>
         <FillerText />
         <FillerText />
       </div>
