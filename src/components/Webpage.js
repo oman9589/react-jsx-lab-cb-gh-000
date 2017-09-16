61fc49ac93842db3de25162bbfcf5456cc056e22
Oliver Spring <oliver9589@gmail.com>
2017-09-16 23:11:11 +0000

    Done.

---
+++ src/components/Webpage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import FillerText from './FillerText';
+
+class Webpage extends React.Component {
+  render() {
+    return (
+      <div className='page'>
+        <h1>The world's coolest webpage</h1>
+        <FillerText />
+        <FillerText />
+      </div>
+    )
+  }
+}
+
+export default Webpage;
