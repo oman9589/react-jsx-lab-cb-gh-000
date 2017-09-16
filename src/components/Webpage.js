105ea97e183b63b8c2e84faa4d64f884d674b3c4
Oliver Spring <oliver9589@gmail.com>
2017-09-16 22:54:30 +0000

    automatically backed up by learn

--- src/components/Webpage.js
+++ src/components/Webpage.js
@@ -3,7 +3,7 @@ import React from 'react';
 class Webpage extends React.Component {
   render() {
     return (
-      <h1 "The worlds coolest webpage"> </h1>
+      <h1 text="The worlds coolest webpage"> </h1>
     )
   }
 }
